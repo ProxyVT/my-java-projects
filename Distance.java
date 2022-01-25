dce37c557f4317eedac447076dfe4ab1d2db7181
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-01-25 17:31:35 +0300

    Update and rename Distance to Distance.java

--- Distance.java
+++ Distance.java
@@ -7,11 +7,11 @@ public class Distance {
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter the distance between two cities in centimeters: ");
         int a = in.nextInt();
-		int centimeters = a;
-		double meters = centimeters * 0.01;
-		double kilometers = meters * 0.001;
+	int centimeters = a;
+	double meters = centimeters * 0.01;
+	double kilometers = meters * 0.001;
         
-		System.out.printf("Distance in centimeters: %,d \n", centimeters);
+	System.out.printf("Distance in centimeters: %,d \n", centimeters);
         System.out.printf("Distance in meters: %,.2f \n", meters);
         System.out.printf("Distance in kilometers: %,.5f \n", kilometers);
 		

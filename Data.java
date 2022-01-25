ac35ddc4c866db94f600f416ffb67f54cedd99f5
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-01-25 17:26:30 +0300

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+   
+    public static void main(String[] args) {
+	
+	String Fn = "Full name: Tiknyus Vladislav Viktorovich"; // Full name of the student 
+	String G = "Group: QA2021"; // Testing group in which the student is studying  
+	String Ma = "Main activity: Junior Research Fellow at the Institute of Economics of the National Academy of Sciences of Belarus"; // The main activity of the student
+	String H = "Hobbies: Gaming, flashing smartphones, carving figures"; // Student's hobbies
+	String Ls = "Life slogan: Nothing is true, everthing is permitted"; // Student's life slogan 
+	
+    System.out.printf(" %s\n %s\n %s\n %s\n %s\n", Fn, G, Ma, H, Ls);
+    }
+}

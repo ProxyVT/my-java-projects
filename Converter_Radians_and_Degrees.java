ba9532bcf46e7d279ae534711c319b7ea7c9d2fc
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-01 12:28:28 +0300

    Update Converter_Radians_and_Degrees.java

--- Converter_Radians_and_Degrees.java
+++ Converter_Radians_and_Degrees.java
@@ -4,17 +4,17 @@ public class Converter_Radians_and_Degrees {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Select the angle measurement type (radians or degrees) and enter a number");
+		System.out.print("Select the angle measurement type (radians or degrees) and enter a number: ");
 		
-		double number = scanner.nextInt();
-		double one_degree = 180 / Math.PI;
-		double one_radian = Math.PI / 180;
-		double degrees = number * one_degree;
-		double radians = number * one_radian;
+		double number = scanner.nextInt();   // Вводимое число радиан или градусов
+		double one_radian = 57.295780;      // Количество градусов в одном радиане
+		double one_degree = 0.017453;	   // Количество радиан в одном градусе
+		double degrees = number * one_radian;	// Формула перевода из радиан в градусы
+		double radians = number * one_degree;	// Формула перевода из градусов в радианы
 		
-		System.out.printf("%f degrees is %f radians\n", number, radians);
-		System.out.printf("%f radians is %f degrees", number, degrees);
-
+		System.out.printf("%f radians is %f degrees\n", number, degrees);
+		System.out.printf("%f degrees is %f radians", number, radians);
+		
 		scanner.close();
 
 	}

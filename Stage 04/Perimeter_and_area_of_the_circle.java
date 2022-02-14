ed87d265190e996d0fe728d53a7c991299c759fd
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-14 15:55:54 +0300

    Update Perimeter_and_area_of_the_circle.java

--- Stage 04/Perimeter_and_area_of_the_circle.java
+++ Stage 04/Perimeter_and_area_of_the_circle.java
@@ -8,12 +8,11 @@ public class Perimeter_and_area_of_the_circle {
 		System.out.println("Enter the radius of the circle: ");
 
 		double radius = scanner.nextInt();
-		double circle_perimeter = 2 * Math.PI * radius; // Формула периметра круга
-		double circle_area = Math.PI * Math.; // Формула площади круга
+		double circle_perimeter = 2 * Math.PI * radius; // Формула периметра круга (длинна окружности)
+		double circle_area = Math.PI * radius * radius; // Формула площади круга
 
-		System.out.printf("Perimeter: %f\nArea: %f", square_perimeter, square_area);
+		System.out.printf("Perimeter (circumference length): %f\nArea: %f", circle_perimeter, circle_area);
 		scanner.close();
 
 	}
-
 }

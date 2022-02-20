e50a49a596dbe7aa7d015b94400d7f1e4a026115
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-20 13:43:33 +0300

    Update Circle.java

--- Stage 04/Circle.java
+++ Stage 04/Circle.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Circle {
 
-	public static void main(String[] args) {
-		
+	public static String resultCircle() {
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the radius of the circle: ");
 
 		double radius = scanner.nextInt();
 		double circle_perimeter = 2 * Math.PI * radius; // Формула периметра круга (длинна окружности)
-		double circle_area = Math.PI * radius * radius; // Формула площади круга
+		double circle_area = Math.PI * Math.pow(radius, 2); // Формула площади круга
 
 		System.out.printf("Perimeter (circumference length): %f\nArea: %f", circle_perimeter, circle_area);
 		scanner.close();
 
+		return null;
+
 	}
-	
+
 }

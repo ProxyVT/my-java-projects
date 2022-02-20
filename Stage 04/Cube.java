7e43dd433d04e6d3649602667d4e91d3f099dc38
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-20 13:44:04 +0300

    Update Cube.java

--- Stage 04/Cube.java
+++ Stage 04/Cube.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Cube {
 
-	public static void main(String[] args) {
-		
+	public static String resultCube() {
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the edge of the cube: ");
-
 		double edge = scanner.nextInt();
-		double voulme = edge * edge * edge; // Формула объема куба
-		double surface = 6 * edge * edge; // Формула площади поверхности куба
+
+		double voulme = Math.pow(edge, 3); // Формула объема куба
+		double surface = 6 * Math.pow(edge, 2); // Формула площади поверхности куба
 
 		System.out.printf("Volume: %f\nSurface area: %f", voulme, surface);
 		scanner.close();
 
+		return null;
+
 	}
-	
+
 }

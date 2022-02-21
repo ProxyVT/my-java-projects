7b1e1dec2d725906d72ba6530876e8f24c97218d
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-21 11:22:17 +0300

    Update Perimeter_and_area_of_the_triangle.java

--- Stage 04/Perimeter_and_area_of_the_triangle.java
+++ Stage 04/Perimeter_and_area_of_the_triangle.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 
-public class Perimeter_and_area_of_the_triangle {
+public class Triangle {
 
-	public static void main(String[] args) {
+	public static String resultTriangle() {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter coordinate x1 and y1  of the triangle: ");
@@ -16,13 +16,16 @@ public class Perimeter_and_area_of_the_triangle {
 		double x3 = scanner.nextInt();
 		double y3 = scanner.nextInt();
 		
-		double side_xy1_xy2 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1)); // Формула нахождения 1-й стороны треугольника
-		double side_xy2_xy3 = Math.sqrt((x3-x2) * (x3-x2) + (y3-y2) * (y3-y2)); // Формула нахождения 2-й стороны треугольника
-		double side_xy3_xy1 = Math.sqrt((x3-x1) * (x3-x1) + (y3-y1) * (y3-y1)); // Формула нахождения 3-й стороны треугольника
-		double triangle_perimeter = side_xy1_xy2 + side_xy2_xy3 + side_xy3_xy1; // Формула периметра треугольника
-		double triangle_area = 0.5 * Math.abs((x2-x1) * (y3-y1) - (x3-x1)*(y2-y1)); // Формула площади треугольника
-		System.out.printf("Perimeter: %f\nArea: %f", triangle_perimeter, triangle_area);
+		double sideOne = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2); // Формула нахождения 1-й стороны треугольника
+		double sideTwo = Math.sqrt(Math.pow((x3-x2), 2) + Math.pow((y3-y2), 2); // Формула нахождения 2-й стороны треугольника
+		double sideThree = Math.sqrt(Math.pow((x3-x1), 2) + Math.pow((y3-y1), 2); // Формула нахождения 3-й стороны треугольника
+		double trianglePerimeter = sideOne + sideTwo + sideThree; // Формула периметра треугольника
+		double triangleArea = 0.5 * Math.abs((x2-x1) * (y3-y1) - (x3-x1)*(y2-y1)); // Формула площади треугольника
+		
+		System.out.printf("Perimeter: %f\nArea: %f", trianglePerimeter, triangleArea);
 		scanner.close();
+		
+		return null;
 
 	}
 

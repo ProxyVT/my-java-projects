0a5b5d262520ba6df021d7edcb4ce614f60732db
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-21 11:27:57 +0300

    Update Rectangle.java

--- Stage 04/Rectangle.java
+++ Stage 04/Rectangle.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Rectangle {
 
-	public static void main(String[] args) {
+	public static String resultRectangle() {
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter sides A and B of the rectangle: ");
-		
 		double sideA = scanner.nextInt();
 		double sideB = scanner.nextInt();
+		
 		double perimeter = 2 * (sideA + sideB); // Формула периметра прямоугольника
 		double area = sideA * sideB; // Формула площади прямоугольника
 		
 		System.out.printf("Perimeter: %f\nArea: %f", perimeter, area);
 		scanner.close();
+		
+		return null;
 
 	}
 	

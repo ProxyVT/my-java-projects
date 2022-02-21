130f282e6cf8cb284cafaaccdeee6227105ae4e0
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-21 11:51:48 +0300

    Update Square.java

--- Stage 04/Square.java
+++ Stage 04/Square.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Square {
 
-	public static void main(String[] args) {
+	public static String resultSquare() {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter side A of the square: ");
 
 		double sideA = scanner.nextInt();
-		double perimeter = sideA * 4; // Формула периметра квадрата
-		double area = sideA * sideA; // Формула площади квадрата
+		double squarePerimeter = sideA * 4; // Формула периметра квадрата
+		double squareArea = Math.pow(sideA, 2); // Формула площади квадрата
 
-		System.out.printf("Perimeter: %f\nArea: %f", perimeter, area);
+		System.out.printf("Perimeter: %f\nArea: %f", squarePerimeter, squareArea);
 		scanner.close();
+		
+		return null;
 
 	}
 	

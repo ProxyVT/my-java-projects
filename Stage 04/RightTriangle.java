bf52ee9de5db1be915ca791fe6eaac5767a6f7a3
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-02-21 12:26:36 +0300

    Update RightTriangle.java

--- Stage 04/RightTriangle.java
+++ Stage 04/RightTriangle.java
@@ -6,12 +6,12 @@ public class RightTriangle {
 			
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter legs A and B of the right triangle: ");
-		double legA = scanner.nextInt();
-		double legB = scanner.nextInt();
+		double legA = scanner.nextDouble();
+		double legB = scanner.nextDouble();
 			
 		double hypotenuse = Math.sqrt(Math.pow(legA, 2) + Math.pow(legB, 2));
-		double rightTriangelPerimeter = legA + legB + hypotenuse; // Формула периметра треугольника
-		double rightTriangleArea = 0.5 * (legA * legB); // Формула площади треугольника
+		double rightTriangelPerimeter = legA + legB + hypotenuse; // Формула периметра прямоугольного треугольника
+		double rightTriangleArea = 0.5 * (legA * legB); // Формула площади прямоугольного треугольника
 			
 		System.out.printf("Hypotenuse: %f\nPerimeter: %f\nArea: %f", hypotenuse, rightTriangelPerimeter, rightTriangleArea);
 		scanner.close();

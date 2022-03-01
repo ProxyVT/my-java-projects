dbf05092e866c1d61d3795d2b362e45465ed2921
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-03-01 23:29:04 +0300

    Create Equation.java

---
+++ Java Tasks/Stage 05/Linear Equation/Equation.java
@@ -0,0 +1,14 @@
+
+public class Equation {
+
+	public static double values(double A, double B) {
+
+		return -B / A; // Нахождение x в линейном уравнении Ax+B=0
+	}
+
+	public static boolean error(double A) {
+
+		return A != 0; // Проверка на неравенство коэффициента A нулю
+	}
+
+}

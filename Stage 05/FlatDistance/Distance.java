2d595454f17d1e334a505a2d104e78700da879b3
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-03-01 13:21:36 +0300

    Create Distance.java

---
+++ Stage 05/FlatDistance/Distance.java
@@ -0,0 +1,10 @@
+
+public class Distance {
+
+	public static double calculation(double x1, double y1, double x2, double y2) {
+
+		return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)); // Формула расстояния между координатами на плоскости
+
+	}
+
+}

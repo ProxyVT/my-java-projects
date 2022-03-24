4d22084ff51dd9aa9a10e96af0f30915836dc970
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-03-24 21:48:37 +0300

    Update CheckAllNumbers.java

--- Java Tasks/Stage 06/Even And Odd Numbers/Check All Numbers/CheckAllNumbers.java
+++ Java Tasks/Stage 06/Even And Odd Numbers/Check All Numbers/CheckAllNumbers.java
@@ -1,7 +1,17 @@
 
 public class AllNumbers {
 
-	public static boolean evenOrOdd(int a, int b, int c) {
-		return a % 2 == 0 && b % 2 == 0 && c % 2 == 0 ;
+	public static boolean checkAll(int a, int b, int c) {
+		return a % 2 == 0 && b % 2 == 0 && c % 2 == 0 
+				|| a % 2 != 0 && b % 2 != 0 && c % 2 != 0;
 	}
+
+	public static boolean checkEven(int a, int b, int c) {
+		return a % 2 == 0 && b % 2 == 0 && c % 2 == 0;
+	}
+
+	public static boolean checkOdd(int a, int b, int c) {
+		return a % 2 != 0 && b % 2 != 0 && c % 2 != 0;
+	}
+	
 }

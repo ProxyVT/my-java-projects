478a72c7e945e81236c058b3638da9656adf53c0
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-03-24 21:56:19 +0300

    Update Main.java

--- Java Tasks/Stage 06/Even And Odd Numbers/Check All Numbers/Main.java
+++ Java Tasks/Stage 06/Even And Odd Numbers/Check All Numbers/Main.java
@@ -10,13 +10,15 @@ public class Main {
 		int b = scanner.nextInt();
 		int c = scanner.nextInt();
 
-		boolean parity = CheckNumbers.evenOrOdd(a, b, c);
-		boolean sameness = CheckNumbers.parity(a, b, c);
-
-		String result1 = parity ? "More even numbers" : "More odd numbers";
-		String result2 = sameness ? "Numbers have the same parity" 
-				: "Numbers have different parity";
-		String msg = String.format("%s\n" + "%s", result1, result2);
+		boolean resultAllNumbers = AllNumbers.checkAll(a, b, c);
+		boolean resultEvenNumbers = AllNumbers.checkEven(a, b, c);
+		boolean resultOddumbers = AllNumbers.checkOdd(a, b, c);
+		String all = resultAllNumbers ? "All numbers are identical in parity" 
+			: "Numbers have different parity";
+		String even = resultEvenNumbers ? "All numbers even" : "";
+		String odd = resultOddumbers ? "All numbers odd" : "";
+		
+		String msg = String.format("%s\n" + "%s" + "%s", all, even, odd);
 		System.out.println(msg);
 
 		scanner.close();

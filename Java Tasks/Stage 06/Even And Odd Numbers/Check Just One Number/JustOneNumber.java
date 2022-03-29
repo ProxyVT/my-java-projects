fd1a16c8157409dd93cec0887083233d9c486fcf
ProxyVT <86965169+ProxyVT@users.noreply.github.com>
2022-03-29 13:08:40 +0300

    Create JustOneNumber.java

---
+++ Java Tasks/Stage 06/Even And Odd Numbers/Check Just One Number/JustOneNumber.java
@@ -0,0 +1,13 @@
+
+public class JustOneNumber {
+
+	public static boolean checkEven(int a, int b, int c) {
+		return a % 2 == 0 || b % 2 == 0 || c % 2 == 0;
+	}
+
+	public static boolean checkOdd(int a, int b, int c) {
+		return a % 2 != 0 || b % 2 != 0 || c % 2 != 0;
+	}
+	
+}
+
